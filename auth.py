28b540a9098935404bed90e882bdb02c073e4991
Michelle Ly <michellely825@gmail.com>
2026-09-14 22:39:56 -0700

    fix: remove encoding passwords from pytest fxs

--- auth.py
+++ auth.py
@@ -11,6 +11,6 @@ def hash_password(password):
 
 
 def verify_password(password, hashed_password):
-    password = password.encode("utf-8")
-    hashed_password = hashed_password.encode("utf-8")
-    return bcrypt.checkpw(password, hashed_password)
+    password_bytes = password.encode("utf-8")
+    hashed_password_bytes = hashed_password.encode("utf-8")
+    return bcrypt.checkpw(password_bytes, hashed_password_bytes)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,16 +1,26 @@
-import bcrypt
+import pytest
 from auth import hash_password, verify_password
+from main import generate_token, verify_token
 
 
 def test_hash_and_verify_correct_password():
     password = "password123"
     hashed_password = hash_password(password)
-    assert verify_password(password.encode("utf-8"), hashed_password.encode("utf-8"))
+    assert verify_password(password, hashed_password)
 
 
 def test_hash_and_verify_incorrect_password():
     password = "password123"
     hashed_password = hash_password(password)
-    assert not verify_password(
-        "wrongpassword".encode("utf-8"), hashed_password.encode("utf-8")
-    )
+    assert not verify_password("wrongpassword", hashed_password)
+
+
+# # this fx runs before each fx that calls it as a param
+# @pytest.fixture
+# def payload_param():
+#     return {"username": ""}
+
+
+# def test_generate_token():
+#     token = generate_token(payload={"user_id": 1, "username": "user1"})
+#     assert verify_token(token) == {"user_id": 1, "username": "user1"}
