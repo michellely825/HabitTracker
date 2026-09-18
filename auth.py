9b2bd1af629145a03470afbc8a16e69c5c8226be
Michelle Ly <michellely825@gmail.com>
2026-09-17 23:19:08 -0700

    refactor: change verify_token() to accept Request instead of token string

--- auth.py
+++ auth.py
@@ -1,4 +1,12 @@
+from fastapi import HTTPException, Request
+
+import jwt
 import bcrypt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
 
 def hash_password(password):
@@ -14,3 +22,19 @@ def verify_password(password, hashed_password):
     password_bytes = password.encode("utf-8")
     hashed_password_bytes = hashed_password.encode("utf-8")
     return bcrypt.checkpw(password_bytes, hashed_password_bytes)
+
+
+def generate_token(payload):
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+
+def verify_token(request: Request) -> dict:
+    auth_header = request.headers["Authorization"]
+    token = auth_header.split(" ")[1]
+    try:
+        decoded_payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return decoded_payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Expired token.")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token.")
